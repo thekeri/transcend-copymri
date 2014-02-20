3eee85da70d4f687d04702e520ead497b9a7c65a
Keri Garel <thekeri@mit.edu>
2014-02-20 10:54:14 -0500

    Updated parameter input needed for DICOMs

--- copymri.py
+++ copymri.py
@@ -35,26 +35,26 @@ if args.trpsubj:
     print("TRP subject number exists")
 
 ### required and optional parameters (subject ID, etc.) for us to grab the DICOMs
-##trpsubj = str(raw_input('Enter TRP subject ID (if none, type "none"): '))
-##pingsubj = str(raw_input('Enter PING subject ID (if none, type "none"): '))
-##if trpsubj.lower() == 'none' and pingsubj.lower() == 'none': #edit to account for leaving them blank
-##    print "You need either a TRP or a PING subject number. Please try again."
-##    trpsubj = str(raw_input('Enter TRP subject ID (if none, type "none"): '))
-##    pingsubj = str(raw_input('Enter PING subject ID (if none, type "none"): '))
-##subject_type = str(raw_input('TD or ASD? '))
-##visit = int(raw_input('Enter MRI visit number (1, 2...): '))
-##run = int(raw_input('Enter run number: '))
-##altsubj = str(raw_input('Enter alternate subject ID (if none, type "none"): ')) #for subject numbers mistyped during scans
-##executor_email = str(raw_input('Enter your email address: ')) #to send message upon completion of script
-##
-###may want to change this to single subject input, with type determination from there (see notes)
-##if altsubj.lower != 'none':
-##    dcmsubj = altsubj
-##elif pingsubj.lower() != 'none':
-##    dcmsubj = pingsubj
-##else:
-##    dcmsubj = trpsubj
-##print dcmsubj #currently printing none, figure out why
+trpsubj = str(raw_input('Enter TRP subject ID (if none, type "none"): '))
+pingsubj = str(raw_input('Enter PING subject ID (if none, type "none"): '))
+if trpsubj.lower() == 'none' and pingsubj.lower() == 'none': #edit to account for leaving them blank
+    print "You need either a TRP or a PING subject number. Please try again."
+    trpsubj = str(raw_input('Enter TRP subject ID (if none, type "none"): '))
+    pingsubj = str(raw_input('Enter PING subject ID (if none, type "none"): '))
+subject_type = str(raw_input('TD or ASD? '))
+visit = int(raw_input('Enter MRI visit number (1, 2...): '))
+run = int(raw_input('Enter run number: '))
+altsubj = str(raw_input('Enter alternate subject ID (if none, type "none"): ')) #for subject numbers mistyped during scans
+executor_email = str(raw_input('Enter your email address: ')) #to send message upon completion of script
+
+#may want to change this to single subject input, with type determination from there (see notes)
+if altsubj.lower != 'none':
+    dcmsubj = altsubj
+elif pingsubj.lower() != 'none':
+    dcmsubj = pingsubj
+else:
+    dcmsubj = trpsubj
+print dcmsubj #currently printing none, figure out why
 ##if subject_type.lower() != 'td' and subject_type.lower() != 'asd':
 ##    print "Please only enter 'TD' or 'ASD' for subject type."
 ##    subject_type = str(raw_input('TD or ASD? '))
